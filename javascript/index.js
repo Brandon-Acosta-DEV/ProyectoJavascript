18ebc24384b0421df5ef303f20e42589b4518473
Brandon Acosta <brandon44acosta@gmail.com>
2023-03-14 16:12:12 -0300

    libreria,fetch,api

--- javascript/index.js
+++ javascript/index.js
@@ -1,11 +1,8 @@
 const contenedorProductos = document.getElementById("contenedor-productos");
-
 const contenedorCarrito = document.getElementById("carrito-contenedor");
-
 const botonVaciar = document.getElementById("vaciar-carrito");
 //MODIFICAR LOS CONTADORES
 const contadorCarrito = document.getElementById("contadorCarrito");
-
 //CALCULAR EL TOTAL
 const cantidad = document.getElementById("cantidad");
 const precioTotal = document.getElementById("precioTotal");
@@ -23,6 +20,24 @@ document.addEventListener("DOMContentLoaded", () => {
 botonVaciar.addEventListener("click", () => {
   carrito.length = 0;
   actualizarCarrito();
+  document.querySelector("carrito");
+  swal({
+    title: "¿Estas seguro?",
+    text: "Se eliminarán todos los productos del carrito",
+    icon: "warning",
+    buttons: true,
+    dangerMode: true,
+  }).then((willDelete) => {
+    if (willDelete) {
+      swal("¡Se eliminaron todos los productos del carrito!", {
+        icon: "success",
+      });
+    } else {
+      swal("¡Sus productos siguen en el carrito!", {
+        icon: "error",
+      });
+    }
+  });
 });
 
 //INYECTO EL HTML
@@ -30,9 +45,9 @@ stockProductos.forEach((producto) => {
   const div = document.createElement("div");
   div.classList.add("producto");
   div.innerHTML = `
-    <img src=${producto.img} alt= "">
+    <img class="img__pto" src=${producto.img} alt= "">
     <h3>${producto.nombre}</h3>
-    <p>${producto.desc}</p>
+    <p class="descProducto">${producto.desc}</p>
     <p class="precioProducto">Precio:$ ${producto.precio}</p>
     <a class="boton-enlace" href="${producto.wapp}" target="_blank">Agendar por Whatsapp</a> <i class="bi bi-whatsapp"></i>
     <button id="agregar${producto.id}" class="boton-agregar">Agregar <i class="fas fa-shopping-cart"></i></button>
@@ -48,7 +63,22 @@ stockProductos.forEach((producto) => {
   boton.addEventListener("click", () => {
     //esta funcion ejecuta el agregar el carrito con la id del producto
     agregarAlCarrito(producto.id);
-    //
+    // Toastify({
+    //   text: "Usted a agregado a su carrito `${producto.nombre}`",
+    //   duration: 2000,
+    //   offset: {
+    //     x: 600,
+    //     y: 30,
+    //   },
+    //   style: {
+    //     background: "linear-gradient(to right, rgb(255, 121, 135), rgb(222, 76, 138))",
+    //   },
+    // }).showToast();
+    swal({
+      title: "¡Excelente!",
+      text: `¡Agregaste a tu carrito ${producto.nombre}!`,
+      icon: "success",
+    });
   });
 });
 
@@ -115,3 +145,39 @@ const actualizarCarrito = () => {
   //Por cada producto q recorro en mi carrito, al acumulador le suma la propiedad precio, con el acumulador
   //empezando en 0.
 };
+
+// Configuración de la clave pública de Stripe
+const publicKey = "pk_test_51MlavdGtoLJ1IUb7T5ro46Otdjuo10tXtv5JdHweaHHkeNisK40dKBQJZaONety5xD4TzmXc2QmHWgVxgkEihTmv003TzDKr7I";
+const stripe = Stripe(publicKey);
+// Crea un elemento de tarjeta de crédito
+var elements = stripe.elements();
+var cardElement = elements.create("card");
+
+// Monta el elemento de la tarjeta de crédito en el DOM
+cardElement.mount("#card-element");
+
+// Manejador de evento para el botón de pago
+document.querySelector("#payment-button").addEventListener("click", async () => {
+  // Creación del token de pago con Stripe
+  const { token, error } = await stripe.createToken("card", cardElement);
+  if (error) {
+    // Manejo del error
+    console.error(error.message);
+  } else {
+    // Envío del token de pago al servidor
+    const response = await fetch("/charge", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        token: token.id,
+        amount: 1000, // Monto en centavos
+      }),
+    });
+
+    const data = await response.json();
+
+    console.log(data);
+  }
+});
